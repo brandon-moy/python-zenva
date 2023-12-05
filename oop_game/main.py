cc0bbd9d53efc13c20741a505c66edd2c646cfa7
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-12-05 15:08:00 -0800

    Defined class for the Room with initializer function, check_code, and get_game_object_names functions and added comments for GameObject methods

--- oop_game/main.py
+++ oop_game/main.py
@@ -21,18 +21,36 @@ class GameObject:
     feel = ""
     smell = ""
 
-# Initialized that sets up the fields of a class
+# Initializer that sets up the fields of a class
     def __init__(self, name, appearance, feel, smell):
         self.name = name
         self.appearance = appearance
         self.feel = feel
         self.smell = smell
         
+# Return string describing object appearance
     def look(self):
         return f"You look at the {self.name}. {self.appearance}\n"
       
+# Return string describing object feel
     def touch(self):
         return f"You touch the {self.name}. {self.feel}\n"
       
+# Return string describing object smell
     def sniff(self):
         return f"You sniff the {self.name}. {self.smell}\n"
+      
+class Room:
+    escape_code = 0
+    game_objects = []
+    
+    def __init__(self, escape_code, game_objects):
+        self.escape_code = escape_code
+        self.game_objects = game_objects
+        
+    def check_code(self, code):
+        return True
+      
+    def get_game_object_names(self):
+        return []
+  
