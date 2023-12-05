366ca622b4411f8c87dd2778815b0d4581d41940
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-12-05 11:59:51 -0800

    Laid out comments of project plan and components

---
+++ oop_game/main.py
@@ -0,0 +1,15 @@
+# Project Definition
+
+# Build an escape room game
+## Room contains several objects
+## Players need a code to escape
+## Objects provide clues about the code
+
+### Each item can be look at, smelled, or touched
+### Each sense provides new information
+### Players discern information based on interaction
+
+### Game components
+# GameObject
+# Room
+# Game
