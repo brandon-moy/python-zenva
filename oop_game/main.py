f47bddaac6947c62921dfd6e4de05e89693b63e1
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-12-05 14:50:58 -0800

    Created initialized function to take in values for name, appearance, feel, and smell and assign to self class

--- oop_game/main.py
+++ oop_game/main.py
@@ -19,3 +19,9 @@ class GameObject:
     appearance = ""
     feel = ""
     smell = ""
+
+def __init__(self, name, appearance, feel, smell):
+    self.name = name
+    self.appearance = appearance
+    self.feel = feel
+    self.smell = smell
