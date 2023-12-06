d7b573325c67ac6fa63f7445dd56669099ba856e
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-12-06 11:42:33 -0800

    Created some tests for the Room to check for matching code and get game object names function

--- oop_game/main.py
+++ oop_game/main.py
@@ -150,3 +150,31 @@ class Game:
       
 game = Game()
 game.take_turn()
+
+# class RoomTests:
+#     def __init__(self):
+#         self.room_1 = Room(111, [
+#             GameObject(
+#                 "Sweater",
+#                 "It's a blue sweater that had the number 12 switched on it.",
+#                 "Someone has unstitched the second number, leaving only the 1.",
+#                 "The sweater smells of laundry detergent."),
+#             GameObject(
+#                 "Chair", 
+#                 "It's a wooden chair with only 3 legs.",
+#                 "Someone had deliberately snapped off one of the legs.",
+#                 "It smells like old wood."),
+#         ])
+#         self.room_2 = Room(222, [])
+        
+#     def test_check_code(self):
+#         print(self.room_1.check_code(111) == True)
+#         print(self.room_1.check_code(222) == False)
+        
+#     def test_get_game_object_names(self):
+#         print(self.room_1.get_game_object_names() == ["Sweater", "Chair"])
+#         print(self.room_2.get_game_object_names() == [])
+        
+# tests = RoomTests()
+# tests.test_check_code()
+# tests.test_get_game_object_names()
