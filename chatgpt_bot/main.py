fc7847905b17995eb909493fe6b15e719c605422
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-12-08 11:24:57 -0800

    Updated import and client passing API key based on updated version of openai-python

--- chatgpt_bot/main.py
+++ chatgpt_bot/main.py
@@ -1,4 +1,4 @@
-import openai
+from openai import OpenAI
 
 
 from dotenv import load_dotenv
@@ -9,7 +9,7 @@ load_dotenv()
 api_key = os.getenv("CHATGPT_API_KEY")
 
 # Pass the API key
-openai.api_key = api_key
+client = OpenAI(api_key=api_key)
 
 # Define prompt
 message = { "role": "user", "content": "why is the sky blue?"}
@@ -18,5 +18,7 @@ messages = []
 messages.append(message)
 
 # Make an API call
+response = openai.create(model="gpt-3.5-turbo", messages=messages)
 
 # Print the response
+print(response)
