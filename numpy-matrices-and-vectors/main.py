b0674bb1e25eb5846eb45161a56e4f7eb6009dd7
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-12-13 11:48:38 -0800

    Used numpy zeros, ones, arange, and linspace methods to print different types of arrays

--- numpy-matrices-and-vectors/main.py
+++ numpy-matrices-and-vectors/main.py
@@ -2,3 +2,15 @@ import numpy as np
 
 numpy_1 = np.array([1, 2, 3])
 print(numpy_1)
+
+zeros_array = np.zeros(3)
+print(zeros_array)
+
+ones_array = np.ones(5)
+print(ones_array)
+
+range_array = np.arange(0, 100, 5)
+print(range_array)
+
+linspace_array = np.linspace(0, 10, 5)
+print(linspace_array)
